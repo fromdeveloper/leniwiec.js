2660fe3000e0fd11b1c7350f713442b08e90f8a7
Przemysław Tyczyński <przem.tycz@outlook.com>
2019-07-11 20:55:59 +0200

    new lib header

--- gulp-tasks/lib.js
+++ gulp-tasks/lib.js
@@ -2,16 +2,21 @@ const gulp = require('gulp');
 const rollup = require('gulp-rollup');
 const rename = require('gulp-rename');
 const uglify = require('gulp-uglify-es').default;
-const headerComment = require('gulp-header-comment');
+const header = require('gulp-header');
 const babel = require('gulp-babel');
 const del = require('del');
 
-const header = `
-	License: <%= pkg.license %>
-	Generated on <%= moment().format('YYYY/MM/DD HH:mm') %>
-	Author: <%= pkg.author.name %> | <%= pkg.author.url %>
-	Copyright (c) 2019 <%= pkg.author.name %>
-`;
+const pkg = require('../package.json');
+const banner = [
+	'/**',
+	' * @package <%= pkg.name %> - <%= pkg.description %>',
+	' * @version v<%= pkg.version %>',
+	' * @link <%= pkg.homepage %>',
+	' * @author <%= pkg.author.name %> | <%= pkg.author.url %>',
+	' * @license <%= pkg.license %>',
+	' */',
+	'',
+].join('\n');
 
 function umd() {
 	process.env.NODE_ENV = 'release';
@@ -26,13 +31,13 @@ function umd() {
 		)
 		.pipe(babel())
 		.pipe(rename('leniwiec.js'))
-		.pipe(headerComment(header))
+		.pipe(header(banner, { pkg }))
 		.pipe(gulp.dest('./dist'))
 
 		.pipe(uglify())
 		.pipe(rename('leniwiec.min.js'))
 
-		.pipe(headerComment(header))
+		.pipe(header(banner, { pkg }))
 		.pipe(gulp.dest('./dist'));
 }
 
